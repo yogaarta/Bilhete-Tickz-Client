3c619b43bf56c4e8346343a104941db02193e8f0
elyasprba <elyaspoerba21@gmail.com>
2022-07-06 19:10:53 +0700

    update tes

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,5 +1,10 @@
 import Styles from '../../styles/Auth.module.css';
 
 export default function Login() {
-   return <h1 className={Styles.tes}>Update Github</h1>;
+   return (
+      <>
+         <h1 className={Styles.tes}>Update Github</h1>
+         <p>Tes</p>
+      </>
+   );
 }
