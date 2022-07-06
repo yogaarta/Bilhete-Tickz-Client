7832a74b53c5d2bce3db63c45c82ef55697b0807
faryadicka <faryadicka98@gmail.com>
2022-07-06 15:49:02 +0700

    setup redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,30 @@
+//Next Componenen
+import Head from 'next/head'
+import Script from 'next/script'
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+//css
 import '../styles/globals.css'
+//Redux
+import { Provider as ReduxProvider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from '../redux/store'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ReduxProvider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+          crossorigin="anonymous" />
+        <Component {...pageProps} />
+      </PersistGate>
+    </ReduxProvider>
+  )
 }
 
 export default MyApp
---
+++ src/redux/store.js
@@ -0,0 +1,19 @@
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { createLogger } from "redux-logger";
+import rpm from "redux-promise-middleware"
+import reducer from "./reducer"
+
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+const logger = createLogger()
+const middlewares = applyMiddleware(rpm, logger)
+
+export const store = createStore(persistedReducer, middlewares)
+export const persistor = persistStore(store)
