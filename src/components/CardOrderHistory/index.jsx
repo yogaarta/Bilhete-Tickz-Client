1d8c185dd9ad9a0163ad6875e8042628309a0657
faryadicka <faryadicka98@gmail.com>
2022-07-07 15:47:46 +0700

    fix order profile + responsive

---
+++ src/components/CardOrderHistory/index.jsx
@@ -0,0 +1,45 @@
+//Import from Package
+import { ChevronDown } from "react-bootstrap-icons";
+//CssModule
+import styles from "../../styles/Profile.module.css";
+//Assets
+import Ebu from "../../assets/icon/ebu.png";
+//Compenents Next
+import Image from "next/image";
+const CardOrderHistory = () => {
+  return (
+    <>
+      <div className={styles.cardOrder}>
+        <div className="d-flex justify-content-between align-items-center pb-2 border-bottom">
+          <div className="col-md-6">
+            <div>
+              <p>Tuesday, 07 July 2020 - 04:30pm</p>
+              <h4>Spider-Man: Homecoming</h4>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div>
+              <Image src={Ebu} alt="abv" />
+            </div>
+          </div>
+        </div>
+        <div className="d-flex justify-content-between align-items-center pt-4">
+          <div className="col-md-6">
+            <div>
+              <button className={styles.buttonActive}>Ticket in Active</button>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className={`position-relative ${styles.ShowDetails}`}>
+              <p>
+                Show Details <ChevronDown />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardOrderHistory;
