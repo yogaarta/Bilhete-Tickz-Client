908f4a984411ac4e10d2710bed30e4909a04085a
elyasprba <elyaspoerba21@gmail.com>
2022-07-11 15:32:24 +0700

    update title header, footer and add SSR page home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,122 +1,121 @@
 //next Component
-import Image from "next/image";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import Image from 'next/image';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 //localcomponent
-import LayoutLoggedIn from "../components/LayoutLoggedIn/LayoutLoggedIn";
+import LayoutLoggedIn from '../components/LayoutLoggedIn/LayoutLoggedIn';
 //cssmodule
-import styles from "../styles/Home.module.css";
+import styles from '../styles/Home.module.css';
 //assets
-import Header from "../assets/img/headerImage.png";
-import Card from "../assets/img/Card.png";
-import NowShowingCard from "../components/NowShowingCard";
-import UpcomingCard from "../components/UpcomingMoviesCard";
+import Header from '../assets/img/headerImage.png';
+// import Card from '../assets/img/Card.png';
+import NowShowingCard from '../components/NowShowingCard';
+import UpcomingCard from '../components/UpcomingMoviesCard';
 // Axios
-import { getNowShowingMoviesAxios, getUpcomingMoviesAxios } from "../modules/movies"
-import { month } from "../modules/dummy"
+// import { getNowShowingMoviesAxios, getUpcomingMoviesAxios } from '../modules/movies';
+// import axios from 'axios';
 
-export default function Home() {
-  // const { loginData } = useSelector((state) => state.auth);
-  const [movies, setMovies] = useState([])
-  const [upMovies, setUpMovies] = useState([])
-  const [errMsg, setErrMsg] =useState("")
-  const router = useRouter();
-  const dispatch = useDispatch();
+export async function getServerSideProps() {
+   const nowShow = await fetch(`${process.env.NEXT_PUBLIC_BE_HOST}/movies/nowshow?page=1&limit=5`);
+   const upComing = await fetch(`${process.env.NEXT_PUBLIC_BE_HOST}/movies/upcoming?page=1&limit=5`);
 
-  useEffect(() => {
-    getNowShowingMoviesAxios()
-    .then((res) => {
-      console.log(res)
-      setMovies(res.data?.data)
-    })
-    .catch((err) => {
-      console.log(err)
-      setErrMsg(err.response?.data.msg)
-    })
-    getUpcomingMoviesAxios()
-    .then((res) => {
-      console.log(res)
-      setUpMovies(res.data?.data)
-    })
-    .catch((err) => {
-      console.log(err)
-      setErrMsg(err.response?.data.msg)
-    })
-  }, []);
+   const dataNowShow = await nowShow.json();
+   const dataUpComing = await upComing.json();
 
+   return {
+      props: {
+         dataNowShow,
+         dataUpComing,
+      },
+   };
+}
 
-  return (
-    <LayoutLoggedIn title="Home">
-      <header className="d-flex flex-column flex-md-row justify-content-center py-5 align-items-center">
-        <div
-          className={`${styles.textHeader} ps-md-4 text-center text-md-start col-md-5 mb-5 mb-md-0`}
-        >
-          <p>Nearest Cinema, Newest Movie.</p>
-          <h1>Find out now!</h1>
-        </div>
-        <div className="col-md-5 text-center">
-          <Image src={Header} alt="header" />
-        </div>
-      </header>
-      <main className={styles.containerHome}>
-        <div className={`d-flex justify-content-between px-5 align-items-center`}>
-          <h4 className={styles.headerNow}>Now Showing</h4>
-          <p
-            onClick={() => {
-              router.push("/movies/nowshowing?page=1");
-            }}
-            className={`${styles.clickAble} text-primary`}
-          >
-            view all
-          </p>
-        </div>
-        <div className={`d-flex justify-content-start my-5 mx-5 gap-4 flex-md-row`}>
-          {movies.map((item) => (
-            <NowShowingCard name={item.name} key={item.id} id={item.id} image={item.img} />
-          ))}
-        </div>
-        <div
-          className={`d-flex justify-content-between px-5 align-items-center ${styles.UpcomingRow}`}
-        >
-          <h4>Upcoming Movies</h4>
-          <p
-            onClick={() => {
-              router.push("/movies/upcoming?page=1");
-            }}
-            className={`${styles.clickAble} text-primary`}
-          >
-            view all
-          </p>
-        </div>
-        {/* <div className="d-flex justify-content-center gap-3 my-5 flex-md-row flex-wrap">
-          {month.map((item) => (
+const Home = ({ dataNowShow, dataUpComing }) => {
+   // const { loginData } = useSelector((state) => state.auth);
+   //  const [movies, setMovies] = useState([]);
+   //  const [upMovies, setUpMovies] = useState([]);
+   //  const [errMsg, setErrMsg] = useState('');
+   const router = useRouter();
+   //  const dispatch = useDispatch();
+
+   //  useEffect(() => {
+   //     getNowShowingMoviesAxios()
+   //        .then((res) => {
+   //           console.log(res);
+   //           setMovies(res.data?.data);
+   //        })
+   //        .catch((err) => {
+   //           console.log(err);
+   //           setErrMsg(err.response?.data.msg);
+   //        });
+   //     getUpcomingMoviesAxios()
+   //        .then((res) => {
+   //           console.log(res);
+   //           setUpMovies(res.data?.data);
+   //        })
+   //        .catch((err) => {
+   //           console.log(err);
+   //           setErrMsg(err.response?.data.msg);
+   //        });
+   //  }, []);
+
+   return (
+      <LayoutLoggedIn title="Home">
+         <header className="d-flex flex-column flex-md-row justify-content-center py-5 align-items-center">
+            <div className={`${styles.textHeader} ps-md-4 text-center text-md-start col-md-5 mb-5 mb-md-0`}>
+               <p>Nearest Cinema, Newest Movie.</p>
+               <h1>Find out now!</h1>
+            </div>
+            <div className="col-md-5 text-center">
+               <Image src={Header} alt="header" />
+            </div>
+         </header>
+         <main className={styles.containerHome}>
+            <div className={`d-flex justify-content-between px-5 align-items-center`}>
+               <h4 className={styles.headerNow}>Now Showing</h4>
+               <p
+                  onClick={() => {
+                     router.push('/movies/nowshowing?page=1');
+                  }}
+                  className={`${styles.clickAble} text-primary`}
+               >
+                  view all
+               </p>
+            </div>
+            <div className={`d-flex justify-content-start my-5 mx-5 gap-4 flex-md-row`}>{dataNowShow.data.length === 0 ? <div>Loading..</div> : dataNowShow.data.map((item) => <NowShowingCard name={item.name} key={item.id} id={item.id} image={item.img} />)}</div>
+            <div className={`d-flex justify-content-between px-5 align-items-center ${styles.UpcomingRow}`}>
+               <h4>Upcoming Movies</h4>
+               <p
+                  onClick={() => {
+                     router.push('/movies/upcoming?page=1');
+                  }}
+                  className={`${styles.clickAble} text-primary`}
+               >
+                  view all
+               </p>
+            </div>
+            <div className="d-flex justify-content-center gap-3 my-5 flex-md-row flex-wrap">
+               {/* {month.map((item) => (
             <button key={item.id} className={styles.buttonMonth}>
               {item.month}
             </button>
-          ))}
-        </div> */}
-        <div className="d-flex justify-content-start my-5 mx-5 gap-4 flex-md-row flex-wrap">
-          {upMovies.map((item) => (
-            <UpcomingCard name={item.name} key={item.id} id={item.id} image={item.img} />
-          ))}
-        </div>
-        <div className="container mt-5 text-center">
-          <div className={styles.CardMember}>
-            Be the vanguard of the <section>Moviegoers</section>{" "}
-            <input
-              type="text"
-              className={`px-3 ${styles.inputMember}`}
-              placeholder="Type your email"
-            />
-            <button className={styles.buttonMember}>Join now</button>
-            <p className={styles.descMember}>
-              By joining you as a Tickitz member, <br />
-              we will always send you the latest updates via email .
-            </p>
-          </div>
-        </div>
-      </main>
-    </LayoutLoggedIn>
-  );
-}
+          ))} */}
+            </div>
+            <div className="d-flex justify-content-start my-5 mx-5 gap-4 flex-md-row flex-wrap">{dataUpComing.data?.length === 0 ? <div>Loading..</div> : dataUpComing.data?.map((item) => <UpcomingCard name={item.name} key={item.id} id={item.id} image={item.img} />)}</div>
+            <div className="container mt-5 text-center">
+               <div className={styles.CardMember}>
+                  Be the vanguard of the <section>Moviegoers</section> <input type="text" className={`px-3 ${styles.inputMember}`} placeholder="Type your email" />
+                  <button className={styles.buttonMember}>Join now</button>
+                  <p className={styles.descMember}>
+                     By joining you as a Tickitz member, <br />
+                     we will always send you the latest updates via email .
+                  </p>
+               </div>
+            </div>
+         </main>
+      </LayoutLoggedIn>
+   );
+};
+
+export default Home;
--- src/pages/movies/[id].jsx
+++ src/pages/movies/[id].jsx
@@ -1,208 +1,170 @@
 //Module
-import { GeoAlt, ChevronDown } from "react-bootstrap-icons";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import { GeoAlt, ChevronDown } from 'react-bootstrap-icons';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 //Components Next
-import Image from "next/image";
+import Image from 'next/image';
 //Components Local
-import LayoutLoggedIn from "../../components/LayoutLoggedIn/LayoutLoggedIn";
+import LayoutLoggedIn from '../../components/LayoutLoggedIn/LayoutLoggedIn';
 //Assets
-import Loading from "../../assets/icon/loading.gif";
+import Loading from '../../assets/icon/loading.gif';
 //CssModule
-import styles from "../../styles/Movies.module.css";
-import CardCinema from "../../components/CardCinemas";
-import { getMoviesDetailAxios, getShowTimesAxios } from "../../modules/movies";
+import styles from '../../styles/Movies.module.css';
+import CardCinema from '../../components/CardCinemas';
+import { getMoviesDetailAxios, getShowTimesAxios } from '../../modules/movies';
 
 const MovieDetail = () => {
-  const [dropdown, setDropdown] = useState();
-  const [movies, setMovies] = useState([]);
-  const [cinemas, setCinemas] = useState([]);
-  const [date, setDate] = useState("");
-  const [errMsg, setErrMsg] = useState("");
-  const router = useRouter();
-  const {
-    id = "",
-    location = "",
-    sort = "",
-    order = "",
-    page = "",
-  } = router.query;
-  useEffect(() => {
-    getMoviesDetailAxios(id)
-      .then((res) => {
-        setMovies(res.data?.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [router]);
+   const [dropdown, setDropdown] = useState();
+   const [movies, setMovies] = useState([]);
+   const [cinemas, setCinemas] = useState([]);
+   const [date, setDate] = useState('');
+   const [errMsg, setErrMsg] = useState('');
+   const router = useRouter();
+   const { id = '', location = '', sort = '', order = '', page = '' } = router.query;
+   useEffect(() => {
+      getMoviesDetailAxios(id)
+         .then((res) => {
+            setMovies(res.data?.data);
+         })
+         .catch((err) => {
+            console.log(err);
+         });
+   }, [router]);
 
-  useEffect(() => {
-    getShowTimesAxios(id, location, date, sort, order, page)
-      .then((res) => {
-        console.log(res);
-        setCinemas(res.data?.data);
-      })
-      .catch((err) => {
-        setErrMsg(err.response?.data.message);
-      });
-    setDropdown(false);
-  }, [router]);
-  console.log(errMsg);
-  return (
-    <LayoutLoggedIn title="Movies Detail">
-      <div className={`container`}>
-        {movies.img ? (
-          <>
-            <div className="d-flex justify-content-between">
-              <div className="col-md-3 col-3 text-center">
-                <div className={styles.cardDetail}>
-                  <Image
-                    src={movies.img ? movies.img : Loading}
-                    width={movies.img ? "236px" : "100px"}
-                    height={movies.img ? "362px" : "100px"}
-                    alt="MovieDetail"
-                  />
-                </div>
-              </div>
-              <div className={`col-md-8 col-8 ${styles.descDetail}`}>
-                <h1>{movies.name ? movies.name : ""}</h1>
-                <p>{movies.category ? movies.category : ""}</p>
-                <div className="mt-2 mt-md-4">
-                  Release date
-                  <section className="text-dark">
-                    {movies.release_date
-                      ? movies.release_date.slice(0, 10)
-                      : ""}
-                  </section>
-                </div>
-                <div className="mt-2 mt-md-4">
-                  Duration
-                  <section className="text-dark">
-                    {movies.duration ? movies.duration : ""}
-                  </section>
-                </div>
-                <div className="mt-2 mt-md-4">
-                  Directed by
-                  <section className="text-dark">
-                    {movies.director ? movies.director : null}
-                  </section>
-                </div>
-                <div className="mt-2 mt-md-4">
-                  Casts
-                  <section className="text-dark">
-                    {movies.cast ? movies.cast : ""}
-                  </section>
-                </div>
-              </div>
-            </div>
-            <div className={`mt-5 ${styles.synopsis}`}>
-              Synopsis
-              <section className="mt-4">
-                {movies.synopsis ? movies.synopsis : ""}
-              </section>
-            </div>
-          </>
-        ) : (
-          <div className="d-flex justify-content-center">
-            <Image src={Loading} />
-          </div>
-        )}
-        <div className={`${styles.showTimes}`}>
-          <h4 className="text-center fw-bold mb-3">Showtimes and Tickets</h4>
-          <div className="d-flex justify-content-center gap-3">
-            <div className={`${styles.dropDate}`}>
-              <input
-                type="date"
-                className={styles.unBlockable}
-                value={date}
-                onChange={(e) => {
-                  setDate(e.target.value);
-                  router.push(`/movies/${router.query.id}?date=${date}`);
-                }}
-              />
-            </div>
-            <div
-              onClick={() => {
-                setDropdown(!dropdown);
-              }}
-              className={`${styles.dropLoc} ${styles.unBlockable}`}
-            >
-              <div className="d-flex justify-content-between">
-                <div className="d-flex align-items-center gap-3">
-                  <GeoAlt />
-                  <div>Location</div>
-                </div>
-                <div>
-                  <ChevronDown />
-                </div>
-              </div>
-              {dropdown ? (
-                <div className={`position-absolute mt-3 ${styles.dropContent}`}>
-                  <div
-                    onClick={(e) => {
-                      e.preventDefault();
-                      if (router.asPath.includes("date")) {
-                        router.push(
-                          `/movies/${router.query.id}?location=jakarta&date=${router.query.date}`
-                        );
-                      }
-                      router.push(
-                        `/movies/${router.query.id}?location=jakarta`
-                      );
-                    }}
-                    className="my-4"
-                  >
-                    Jakarta
+   useEffect(() => {
+      getShowTimesAxios(id, location, date, sort, order, page)
+         .then((res) => {
+            // console.log(res);
+            setCinemas(res.data?.data);
+         })
+         .catch((err) => {
+            setErrMsg(err.response?.data.message);
+         });
+      setDropdown(false);
+   }, [router]);
+   // console.log(errMsg);
+
+   return (
+      <LayoutLoggedIn title="Movies Detail">
+         <div className={`container`}>
+            {movies.img ? (
+               <>
+                  <div className="d-flex justify-content-between">
+                     <div className="col-md-3 col-3 text-center">
+                        <div className={styles.cardDetail}>
+                           <Image src={movies.img ? movies.img : Loading} width={movies.img ? '236px' : '100px'} height={movies.img ? '362px' : '100px'} alt="MovieDetail" />
+                        </div>
+                     </div>
+                     <div className={`col-md-8 col-8 ${styles.descDetail}`}>
+                        <h1>{movies.name ? movies.name : ''}</h1>
+                        <p>{movies.category ? movies.category : ''}</p>
+                        <div className="mt-2 mt-md-4">
+                           Release date
+                           <section className="text-dark">{movies.release_date ? movies.release_date.slice(0, 10) : ''}</section>
+                        </div>
+                        <div className="mt-2 mt-md-4">
+                           Duration
+                           <section className="text-dark">{movies.duration ? movies.duration : ''}</section>
+                        </div>
+                        <div className="mt-2 mt-md-4">
+                           Directed by
+                           <section className="text-dark">{movies.director ? movies.director : null}</section>
+                        </div>
+                        <div className="mt-2 mt-md-4">
+                           Casts
+                           <section className="text-dark">{movies.cast ? movies.cast : ''}</section>
+                        </div>
+                     </div>
                   </div>
-                  <div
-                    onClick={(e) => {
-                      e.preventDefault();
-                      if (router.asPath.includes("date")) {
-                        router.push(
-                          `/movies/${router.query.id}?location=bandung&date=${router.query.date}`
-                        );
-                      }
-                      router.push(
-                        `/movies/${router.query.id}?location=bandung`
-                      );
-                    }}
-                    className="my-4"
-                  >
-                    Bandung
+                  <div className={`mt-5 ${styles.synopsis}`}>
+                     Synopsis
+                     <section className="mt-4">{movies.synopsis ? movies.synopsis : ''}</section>
                   </div>
-                </div>
-              ) : null}
-            </div>
-          </div>
-          <div className="d-flex justify-content-center gap-3 flex-wrap mt-5">
-            {cinemas.length > 0 ? (
-              <>
-                {cinemas.map((item) => (
-                  <CardCinema
-                    key={item.id}
-                    address={item.address}
-                    pictures={item.pictures}
-                    time={item.list_time}
-                    location={item.location}
-                    price={item.price}
-                    name={item.name}
-                  />
-                ))}
-              </>
+               </>
             ) : (
-              <>
-                <h1>...</h1>
-              </>
+               <div className="d-flex justify-content-center">
+                  <Image src={Loading} />
+               </div>
             )}
-          </div>
-        </div>
-        <div className="d-flex justify-content-center mt-4 mt-md-5">
-          <button className={styles.buttonPag}>1</button>
-        </div>
-      </div>
-    </LayoutLoggedIn>
-  );
+            <div className={`${styles.showTimes}`}>
+               <h4 className="text-center fw-bold mb-3">Showtimes and Tickets</h4>
+               <div className="d-flex justify-content-center gap-3">
+                  <div className={`${styles.dropDate}`}>
+                     <input
+                        type="date"
+                        className={styles.unBlockable}
+                        value={date}
+                        onChange={(e) => {
+                           setDate(e.target.value);
+                           router.push(`/movies/${router.query.id}?date=${date}`);
+                        }}
+                     />
+                  </div>
+                  <div
+                     onClick={() => {
+                        setDropdown(!dropdown);
+                     }}
+                     className={`${styles.dropLoc} ${styles.unBlockable}`}
+                  >
+                     <div className="d-flex justify-content-between">
+                        <div className="d-flex align-items-center gap-3">
+                           <GeoAlt />
+                           <div>Location</div>
+                        </div>
+                        <div>
+                           <ChevronDown />
+                        </div>
+                     </div>
+                     {dropdown ? (
+                        <div className={`position-absolute mt-3 ${styles.dropContent}`}>
+                           <div
+                              onClick={(e) => {
+                                 e.preventDefault();
+                                 if (router.asPath.includes('date')) {
+                                    router.push(`/movies/${router.query.id}?location=jakarta&date=${router.query.date}`);
+                                 }
+                                 router.push(`/movies/${router.query.id}?location=jakarta`);
+                              }}
+                              className="my-4"
+                           >
+                              Jakarta
+                           </div>
+                           <div
+                              onClick={(e) => {
+                                 e.preventDefault();
+                                 if (router.asPath.includes('date')) {
+                                    router.push(`/movies/${router.query.id}?location=bandung&date=${router.query.date}`);
+                                 }
+                                 router.push(`/movies/${router.query.id}?location=bandung`);
+                              }}
+                              className="my-4"
+                           >
+                              Bandung
+                           </div>
+                        </div>
+                     ) : null}
+                  </div>
+               </div>
+               <div className="d-flex justify-content-center gap-3 flex-wrap mt-5">
+                  {cinemas.length > 0 ? (
+                     <>
+                        {cinemas.map((item) => (
+                           <CardCinema key={item.id} address={item.address} pictures={item.pictures} time={item.list_time} location={item.location} price={item.price} name={item.name} />
+                        ))}
+                     </>
+                  ) : (
+                     <>
+                        <h1>...</h1>
+                     </>
+                  )}
+               </div>
+            </div>
+            <div className="d-flex justify-content-center mt-4 mt-md-5">
+               <button className={styles.buttonPag}>1</button>
+            </div>
+         </div>
+      </LayoutLoggedIn>
+   );
 };
 
 export default MovieDetail;
